18de8ed383b445d559760ff82798c4100d5c0999
AdamLoginov <mysnikov2000@mail.ru>
2022-08-03 07:42:37 +0300

    comp init

--- VPsite/SYSControl/urls.py
+++ VPsite/SYSControl/urls.py
@@ -5,16 +5,31 @@ from .views import (
     CompanyView,
     StaffView,
     СontractorView,
+    СonfigurationView,
     )
 
 urlpatterns = [
     path('', BaseView.as_view(), name='base'),
+
     path('company/', CompanyView.as_view(), name='company'),
     path('company/create/', CompanyView.create, name='companyCreate'),
     path('company/edit/<int:id>/', CompanyView.edit, name='companyEit'),
     path('company/delete/<int:id>/', CompanyView.delete, name='companyDelete'),
+
     path('contractor/', СontractorView.as_view(), name='contractor'),
     path('contractor/create/', СontractorView.create, name='contractorCreate'),
+    path('contractor/edit/<int:id>/', СontractorView.edit, name='contractorEdit'),
+    path('contractor/delete/<int:id>/', СontractorView.delete, name='contractorDelete'),
+
+    path('configuration/', СonfigurationView.as_view(), name='configuration'),
+    path('configuration/crate/', СonfigurationView.create, name='configurationCreate'),
+    path('configuration/edit/<int:id>/', СonfigurationView.edit, name='configurationEdit'),
+    path('configuration/delete/<int:id>/', СonfigurationView.delete, name='configurationDelete'),
+
+
     path('staff/', StaffView.as_view(), name='staff'),
+    path('staff/create/', StaffView.create, name='staffCreate'),
+    path('staff/changepassword/<int:id>', StaffView.changePassword, name='staffChangePassword'),
+
     # path('staff/<str:name>', ProfileView.as_view(), name='profile')
 ]
--- VPsite/SYSControl/views.py
+++ VPsite/SYSControl/views.py
@@ -1,3 +1,4 @@
+from urllib import request
 from django.views.generic import View
 from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
@@ -9,7 +10,8 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import  (
     HeadDataBase, 
     Company,
-    Сontractor
+    Сontractor,
+    Сonfiguration
 )
 
 User = get_user_model()
@@ -65,6 +67,7 @@ class CompanyView(LoginRequiredMixin, View):
         else:
             return HttpResponseRedirect('errorPage.html')
 
+
 class СontractorView(LoginRequiredMixin, View):
     def get(self, request):
         if request.user.is_superuser:
@@ -90,11 +93,64 @@ class СontractorView(LoginRequiredMixin, View):
         if request.user.is_superuser:
             model = Сontractor.objects.get(id=id)
             if request.method == 'POST':
-                pass
+                model.name = request.POST.get('name')
+                model.cod = request.POST.get('cod')
+                model.inn = request.POST.get('inn')
+                model.kpp = request.POST.get('kpp')
+                model.save()
+                return HttpResponseRedirect('/contractor')
+            else:
+                return render(request, 'contractor_edit.html', {'contractor': model})
+        else:
+            return HttpResponseRedirect('errorPage.html')
+    
+    def delete(request, id):
+        if request.user.is_superuser:
+            model = Сontractor.objects.get(id=id)
+            model.delete()
+            return HttpResponseRedirect('/contractor')
         else:
             return HttpResponseRedirect('errorPage.html')
 
 
+class СonfigurationView(LoginRequiredMixin, View):
+    
+    def get(self, request):
+        if request.user.is_superuser:
+            configuration = Сonfiguration.objects.all()
+            return render(request, 'configuration.html', {'configurations': configuration})
+        else:
+            return HttpResponseRedirect('errorPage.html')
+    
+    def create(request):
+        if request.user.is_superuser:
+            if request.method == "POST":
+                model = Сonfiguration()
+                model.name = request.POST.get('name')
+                model.save()
+            return HttpResponseRedirect("/configuration")
+        else:
+            return HttpResponseRedirect('errorPage.html')
+    
+    def edit(request, id):
+        if request.user.is_superuser:
+            model = Сonfiguration.objects.get(id = id)
+            if request.method == 'POST':
+                model.name = request.POST.get('name')
+                model.save()
+                return HttpResponseRedirect('/configuration')
+            else:
+                return render(request, 'configuration_edit.html', {"configuration": model})
+        else:
+            return HttpResponseRedirect('errorPage.html')
+
+    def delete(request, id):
+        if request.user.is_superuser:
+            model = Сonfiguration.objects.get(id = id)
+            model.delete()  
+            return HttpResponseRedirect('/configuration')
+        else:
+            return HttpResponseRedirect('errorPage.html')
 
 
 class StaffView(LoginRequiredMixin, View):
@@ -112,22 +168,51 @@ class StaffView(LoginRequiredMixin, View):
         
         return render(request, 'staff.html', context)
 
-    def post(self, request, *args, **kwargs):
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect('/')
+    def create(request):
+        if request.user.is_superuser:
+            if request.method == 'POST':
+                model = User()
+                model.username = request.POST.get('username')
+                model.first_name = request.POST.get('nameFirst')
+                model.last_name = request.POST.get('nameLast')
+                model.email = request.POST.get('email')
+                model.set_password(request.POST.get('password'))
+                model.save()
+            return HttpResponseRedirect('/staff')
         else:
+            return HttpResponseRedirect('errorpage.html')
 
-            form = UserCreationForm()
-            users = User.objects.all()
+    def edit(request):
+        if request.user.is_superuser:
+            if request.method == 'POST':
+                pass
+            else:
+                pass
+        else:
+            return HttpResponseRedirect('errorpage.html')
 
+    def changePassword(request, id):
+        if request.user.is_superuser:
+            model = User.objects.get(id=id)
             context = {
-                'users' : users,
-                'form': form,
+                'user': model
             }
+
+            if request.method == 'POST':
+                password_1 = request.POST.get('password1')
+                password_2 = request.POST.get('password2')
+
+                if password_1 == password_2:
+                    model.set_password(password_1)
+                    model.save()
+                    context['message'] = 'Пароль успешно сменен'
+                else:
+                    context['message'] = 'Пароли не совподают'
             
-            return render(request, 'staff.html', context)
+            return render(request, 'password_change.html', context)
+                
+        else:
+            return HttpResponseRedirect('errorPage.html')
 
 class ProfileView(LoginRequiredMixin, View):
     def get(self, request, name, *args, **kwargs):     
